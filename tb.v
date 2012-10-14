7f11fe74723a7db8462536a31e31c09b4ba872e8
David Poole <testcluster@gmail.com>
2012-10-14 10:48:29 -0600

    re-used the name for the 8-bit adder accumulator top

--- tb.v
+++ tb.v
@@ -1,44 +1,95 @@
-`timescale 1 ns/10 ps
 
-module eq2_testbench;
-  reg [1:0] test_in0, test_in1;
-  wire test_out;
-  
-  eq2 uut
-    (.a(test_in0), .b(test_in1), .aeqb(test_out));
-    
-  initial
-  begin
-    test_in0 = 2'b00;
-    test_in1 = 2'b00;
-    # 200;
-    
-    test_in0 = 2'b01;
-    test_in1 = 2'b00;
-    # 200;
-    
-    test_in0 = 2'b01;
-    test_in1 = 2'b11;
-    # 200;
-   
-    test_in0 = 2'b10;
-    test_in1 = 2'b10;
-    # 200;
+`timescale 1 ns / 10 ps
+`define PERIOD 10
 
-    test_in0 = 2'b10;
-    test_in1 = 2'b00;
-    # 200;
+module top_adder_accumulator( MCLK, Led, sw, seg, dp, an, btn );
 
-    test_in0 = 2'b11;
-    test_in1 = 2'b11;
-    # 200;
+    input MCLK; 
+    input [7:0] sw;
+    input [3:0] btn;
 
-    test_in0 = 2'b11;
-    test_in1 = 2'b01;
-    # 200;
+    output reg [7:0] Led;
+    output reg [6:0] seg;
+    output reg dp;
+    output reg [3:0] an;
 
-    $stop;
+//    reg t_clk = 0;
+//    reg t_reset = 1;
+//    reg t_enable = 0;
+//    reg [1:0] t_signal_in;
+//    wire [1:0] t_signal_out;
+
+//    top uut
+//        (.Clock(t_clk),
+//         .Reset(t_reset),
+//         .Enable(t_enable),
+//         .Signal_in(t_signal_in),
+//         .Signal_out(t_signal_out) );
+
+    reg int_load = 0;
+    reg int_reset = 0;
+    reg int_add = 0;
+
+    wire [6:0] int_seg;
+    wire [3:0] int_an;
+    wire int_dp;
+
+    wire [7:0] adder_acc_data_out;
+
+    AdderAccumulator run_adder_accumulator
+        ( .load(int_load),
+          .add(int_add),
+          .reset(int_reset),
+          .clock(MCLK),
+          .data_in(sw),
+          .output_sel(sw[1:0]),
+          .data_out(adder_acc_data_out) );
+
+    digits_to_7seg test_digits_to_7seg 
+        ( .mclk(MCLK),
+          .digit0_in(4'b0000),
+          .byte_in(adder_acc_data_out),
+          .seg(int_seg),
+          .an(int_an),
+          .dp(int_dp) );
+          
+    always @(posedge MCLK)
+    begin
+        int_reset <= btn[0];
+        int_load <= btn[1];
+        int_add <= btn[2];
+
+        seg <= int_seg;
+        an <= int_an;
+        dp <= int_dp;
+
+        Led <= 8'd0;
     end
+
+
+//    always
+//    begin
+//        #`PERIOD t_clk = ~t_clk;
+//    end
+
+//    initial
+//    begin
+//        $display("Hello, world");
+//        t_reset = 1;
+//        t_enable = 0;
+//        t_signal_in = 0;
+//        # 15
+//
+//        t_reset = 0;
+//        # 10;
+//
+//        t_signal_in = 2'b11;
+//        t_enable = 1;
+//        # 10;
+//
+//        t_enable = 0;
+//        # 10;
+//    end
 
 endmodule
 
