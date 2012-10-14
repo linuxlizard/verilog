5ce829d57742757117bae076b570f8473f871be4
David Poole <testcluster@gmail.com>
2012-10-14 10:45:37 -0600

    use an always for the clock instead of generating manuall; add prototype assert macro

--- test_register.v
+++ test_register.v
@@ -9,9 +9,21 @@
 `define PERIOD 10
 `define HALF_PERIOD 5
 
+//`define ASSERT_EQUALS(x,y) x = y
+
+`define ASSERT_EQUALS(x,y) \
+        repeat(1)\
+        begin\
+            if( (x) != (y) ) \
+            begin\
+                $write( "assert failed %d != %d\n", (x), (y) );\
+                $finish_and_return(3);\
+            end\
+        end 
+
 module register_test;
     parameter WIDTH=16;
-    reg t_clk;
+    reg t_clk=0;
     reg t_load;
     reg t_reset;
     reg [WIDTH-1:0] t_data_in;
@@ -31,89 +43,60 @@ module register_test;
          .d(t_data_in),
          .q(t_data_out));
          
-    initial
+    always 
     begin
-//        $dumpfile("register.vcd");
-//        $dumpvars(0,register_test);
+        #`PERIOD t_clk = ~t_clk;
+    end
+        
+//    initial
+    always 
+    begin
+        $dumpfile("test_register.vcd");
+        $dumpvars(0,register_test);
 
         $display("Hello, world");
-        t_clk = 0;
         t_reset = 1;
         t_data_in = 0;
         t_load = 0;
-        # 5;
-
-        t_clk = 1;
-        #5;
+        # 15;
 
         t_reset = 0;
-        # 5;
-
-        t_clk = 0;
-        #5;
+        # 10;
 
         t_load = 1;
         t_data_in = 16'haa;
-        # 5;
+        # 10;
         
-        t_clk = 1;
-        # 5;
-
         /* set data to 0xffff so we can see it doesn't get pulled into 
          * the register
          */
         t_load = 0;
         t_data_in = 16'hffff;
-        # 5;
-
-        t_clk = 0;
-        # 5;
+        # 10;
+//        `line 100;
+//        $display( #line );
+        `ASSERT_EQUALS(t_data_in,16'hfffe)
 
         t_data_in = 16'h4242;
         t_load = 1;
-        #5;
+        # 10;
         
-        t_clk = 1;
-        #5;
-
         /* set data to 0xffff so we can see it doesn't get pulled into 
          * the register
          */
         t_data_in = 16'hffff;
         t_load = 0;
-        #5;
+        #10;
                 
-        t_clk = 0;
-        #5;
+        #20;
 
-        #5;
-
-        t_clk = 1;
-        #5;
-
-        #5;
-
-        t_clk = 0;
-        #5;
-      
         t_reset = 1;
-        #5;
-
-        t_clk = 1;
-        #5;
-
-        #5;
-
-        t_clk = 0;
-        #5;
-
+        #10;
+        
         $display("Goodbye, world");
+        # 100 
+        $finish(2);
     end
 
-//    always @(*)
-//    begin
-//        #`PERIOD t_clk = ~t_clk;
-//    end
-        
 endmodule
 
