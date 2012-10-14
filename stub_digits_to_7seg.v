ee5e4008f3314c997aea2fefd863998de36167c1
David Poole <testcluster@gmail.com>
2012-10-14 10:44:23 -0600

    stub Verilog to stand in for 7seg driver VHDL code

---
+++ stub_digits_to_7seg.v
@@ -0,0 +1,19 @@
+`timescale 1 ns / 10 ps
+
+module digits_to_7seg( mclk, digit0_in, byte_in, seg, an, dp );
+
+    input wire mclk;
+    input wire [3:0] digit0_in;
+    input wire [7:0] byte_in;
+
+    output reg [6:0] seg;
+    output reg [3:0] an;
+    output reg dp;
+
+    always @(posedge mclk)
+    begin
+        seg <= 6'd0;
+    end
+
+endmodule
+
