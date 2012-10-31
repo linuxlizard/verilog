c4dd10028cb76ed84d2bbe6319a905f9d770bfff
David Poole <testcluster@gmail.com>
2012-10-30 19:49:39 -0600

    add display_mask_in to be compatible with actual digits_to_7seg

--- stub_digits_to_7seg.v
+++ stub_digits_to_7seg.v
@@ -1,14 +1,13 @@
 `timescale 1 ns / 10 ps
 
-module stub_digits_to_7seg( mclk, rst, word_in, seg, an, dp );
+module stub_digits_to_7seg( input wire mclk,
+                            input wire rst,
+                            input wire [15:0] word_in,
+                            input wire [7:0] display_mask_in,
 
-    input wire mclk;
-    input wire rst;
-    input wire [15:0] word_in;
-
-    output reg [6:0] seg;
-    output reg [3:0] an;
-    output reg dp;
+                            output reg [6:0] seg,
+                            output reg [3:0] an,
+                            output reg dp );
 
     always @(posedge mclk)
     begin
