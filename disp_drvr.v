bcbda702ac70c3ca1e9cb50947f466fb77495244
David Poole <testcluster@gmail.com>
2012-11-05 19:14:46 -0700

    add snooze +1 minute (should be +5 but only +1 right now)

--- disp_drvr.v
+++ disp_drvr.v
@@ -15,11 +15,69 @@ module DISP_DRVR
     reg [15:0] snooze_alarm_time=0;
     reg snooze_active=0;
 
-    reg int_sound_alarm;
+    reg int_sound_alarm=0;
     reg [15:0] int_display;
 
     assign sound_alarm = int_sound_alarm;
     assign display = int_display;
+
+    task bcd_clock_minute; 
+          input one_minute;
+          input [3:0] ms_hour;
+          input [3:0] ls_hour; 
+          input [3:0] ms_min;
+          input [3:0] ls_min;
+          
+          output reg [3:0] out_ms_hour;
+          output reg [3:0] out_ls_hour; 
+          output reg [3:0] out_ms_min;
+          output reg [3:0] out_ls_min ;
+
+          reg [3:0] int_ms_hour;
+          reg [3:0] int_ls_hour; 
+          reg [3:0] int_ms_min;
+          reg [3:0] int_ls_min ;
+    begin
+            int_ms_hour = ms_hour;
+            int_ls_hour = ls_hour;
+            int_ms_min = ms_min;
+            int_ls_min = ls_min;
+
+            if( one_minute ) 
+            begin
+                int_ls_min = int_ls_min+1;
+                if( int_ls_min == 10 )
+                begin
+                    // ls_minutes overflows to ms_minutes 
+                    int_ls_min = 0;
+                    int_ms_min = int_ms_min+1;
+                    if( int_ms_min == 6 )
+                    begin
+                        // minutes rolls over into hours
+                        int_ms_min = 0;
+                        int_ls_hour = int_ls_hour+1;
+                        if( int_ls_hour==10 )
+                        begin
+                            // ls_hours overflows to ms_hours
+                            int_ls_hour=0;
+                            int_ms_hour = int_ms_hour + 1;
+                        end
+                        else if ( int_ms_hour==2 && int_ls_hour==4 ) 
+                        begin
+                            // rollover midnight to next day (00:00)
+                            int_ls_hour = 0;
+                            int_ms_hour = 0;
+                        end
+                    end
+                end
+            end
+
+            out_ms_hour = int_ms_hour;
+            out_ls_hour = int_ls_hour;
+            out_ms_min = int_ms_min;
+            out_ls_min = int_ls_min;
+    end
+    endtask
 
     always @(one_minute,snooze,stop_alarm,show_alarm,current_time)
     begin
@@ -40,6 +98,9 @@ module DISP_DRVR
                 if( alarm_time==current_time )
                 begin
                     int_sound_alarm <= 1;
+
+                    /* initialize the snooze if we need it later */
+                    snooze_alarm_time <= alarm_time;
                 end
             end
         end
@@ -47,9 +108,18 @@ module DISP_DRVR
 
         if( snooze ) 
         begin
+            // turn off the alarm
+            int_sound_alarm <= 0;
+            
             // push the next_alarm time forward 
             snooze_active <= 1;
-            // TODO add five minutes to snooze time
+            // add five minutes to snooze time brute force badly
+            bcd_clock_minute( 1, alarm_time[15:12], alarm_time[11:8],
+                                alarm_time[7:4], alarm_time[3:0],
+                              snooze_alarm_time[15:12],
+                              snooze_alarm_time[11:8],
+                              snooze_alarm_time[7:4],
+                              snooze_alarm_time[3:0] );
         end
 
         // turn off the alarm
