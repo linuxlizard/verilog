400ee46bceb76f55a46b23c45aadb3aa16965ed7
David Poole <testcluster@gmail.com>
2012-11-05 19:01:23 -0700

    add a set alarm keystroke set; add a single keystroke for timeout

--- PS2_Keyboard.v
+++ PS2_Keyboard.v
@@ -48,6 +48,9 @@ module PS2_Keyboard
 `define KEY_DELAY 500
     initial
     begin
+        /*
+         *      Load Clock Time 12:34
+         */
         # `KEY_DELAY
         ps2_key_code <= `KP_INVALID; 
 
@@ -93,6 +96,7 @@ module PS2_Keyboard
 
         # `KEY_DELAY
 
+        /*  Push '-' to Load Current Time */
         $display( "push the - key to load current time" );
         # `KEY_DELAY
         ps2_key_code <= `KP_MINUS; 
@@ -104,12 +108,86 @@ module PS2_Keyboard
         # `KEY_DELAY
         ps2_key_code <= `KP_INVALID; 
 
+
+        /* 
+         * Load Alarm Time  12:35
+         */
+        # `KEY_DELAY
+        ps2_key_code <= `KP_1; 
+        # `KEY_DELAY
+        ps2_key_code <= `KP_KEY_RELEASED;
+        # `KEY_DELAY
+        ps2_key_code <= `KP_1;
+
+        # `KEY_DELAY
+        ps2_key_code <= `KP_INVALID; 
+
+        # `KEY_DELAY
+        ps2_key_code <= `KP_2; 
+        # `KEY_DELAY
+        ps2_key_code <= `KP_KEY_RELEASED;
+        # `KEY_DELAY
+        ps2_key_code <= `KP_2;
+
+        # `KEY_DELAY
+        ps2_key_code <= `KP_INVALID; 
+
+        # `KEY_DELAY
+        ps2_key_code <= `KP_3; 
+        # `KEY_DELAY
+        ps2_key_code <= `KP_KEY_RELEASED;
+        # `KEY_DELAY
+        ps2_key_code <= `KP_3;
+
+        # `KEY_DELAY
+        ps2_key_code <= `KP_INVALID; 
+
+        # `KEY_DELAY
+        ps2_key_code <= `KP_5; 
+        # `KEY_DELAY
+        ps2_key_code <= `KP_KEY_RELEASED;
+        # `KEY_DELAY
+        ps2_key_code <= `KP_5;
+
+        # `KEY_DELAY
+        ps2_key_code <= `KP_INVALID; 
+
+        # `KEY_DELAY
+
+
+        /*  Push '*' to Load Alarm Time */
+        $display( "push the * key to load alarm time" );
+        # `KEY_DELAY
+        ps2_key_code <= `KP_STAR; 
+        # `KEY_DELAY
+        ps2_key_code <= `KP_KEY_RELEASED;
+        # `KEY_DELAY
+        ps2_key_code <= `KP_STAR;
+        # `KEY_DELAY
+        ps2_key_code <= `KP_INVALID; 
+
+        # `KEY_DELAY
+        # `KEY_DELAY
+        # `KEY_DELAY
+        # `KEY_DELAY
+        # `KEY_DELAY
+
+
+        /* Push a number. Wait for timeout */
+        # `KEY_DELAY
+        ps2_key_code <= `KP_9; 
+        # `KEY_DELAY
+        ps2_key_code <= `KP_KEY_RELEASED;
+        # `KEY_DELAY
+        ps2_key_code <= `KP_9;
+        # `KEY_DELAY
+        ps2_key_code <= `KP_INVALID; 
+
+
         $display( "end of PS2 input" );
 //        $finish;
-//        $;
-        # 10000000;
+//        # 10000000;
     end
-
 
 endmodule
 
