2200e51cdf03527e888541855532ec86e8e73bcf
David Poole <testcluster@gmail.com>
2012-11-07 10:34:08 -0700

    change to blocking combinational logic so can add +5 minutes on snooze

--- disp_drvr.v
+++ disp_drvr.v
@@ -37,27 +37,80 @@ module DISP_DRVR
           reg [3:0] int_ls_hour; 
           reg [3:0] int_ms_min;
           reg [3:0] int_ls_min ;
+    begin
+            int_ms_hour = ms_hour;
+            int_ls_hour = ls_hour;
+            int_ms_min = ms_min;
+            int_ls_min = ls_min;
+
+            int_ls_min = int_ls_min+5;
+            if( int_ls_min > 10 )
+            begin
+                // ls_minutes overflows to ms_minutes 
+                int_ls_min = int_ls_min - 10;
+                int_ms_min = int_ms_min+1;
+                if( int_ms_min == 6 )
+                begin
+                    // minutes rolls over into hours
+                    int_ms_min = 0;
+                    int_ls_hour = int_ls_hour+1;
+                    if( int_ls_hour==10 )
+                    begin
+                        // ls_hours overflows to ms_hours
+                        int_ls_hour=0;
+                        int_ms_hour = int_ms_hour + 1;
+                    end
+                    else if ( int_ms_hour==2 && int_ls_hour==4 ) 
+                    begin
+                        // rollover midnight to next day (00:00)
+                        int_ls_hour = 0;
+                        int_ms_hour = 0;
+                    end
+                end
+            end
+
+            out_ms_hour = int_ms_hour;
+            out_ls_hour = int_ls_hour;
+            out_ms_min = int_ms_min;
+            out_ls_min = int_ls_min;
+    end
+    endtask
+
+    task foo_bcd_clock_minute; 
+          input [3:0] ms_hour;
+          input [3:0] ls_hour; 
+          input [3:0] ms_min;
+          input [3:0] ls_min;
+          
+          output reg [3:0] out_ms_hour;
+          output reg [3:0] out_ls_hour; 
+          output reg [3:0] out_ms_min;
+          output reg [3:0] out_ls_min ;
+
+          reg [3:0] int_ms_hour;
+          reg [3:0] int_ls_hour; 
+          reg [3:0] int_ms_min;
+          reg [3:0] int_ls_min ;
 
           reg [15:0] the_time;
     begin
-            the_time <= { ms_hour, ls_hour, ms_min, ls_min };
+            the_time = { ms_hour, ls_hour, ms_min, ls_min };
 
-            int_ms_hour <= ms_hour;
-            int_ls_hour <= ls_hour;
-            int_ms_min <= ms_min;
-            int_ls_min <= ls_min;
+            int_ms_hour = ms_hour;
+            int_ls_hour = ls_hour;
+            int_ms_min = ms_min;
+            int_ls_min = ls_min;
 
             if( the_time==16'h2359 ) begin
                 // rollover midnight to next day (00:00)
-                int_ms_hour <= 4'd0;
-                int_ls_hour <= 4'd0;
-                int_ms_min <= 4'd0;
-                int_ls_min <= 4'd4;
-
-                the_time <= the_time + 4'd5;
+                int_ms_hour = 4'd0;
+                int_ls_hour = 4'd0;
+                int_ms_min = 4'd0;
+                int_ls_min = 4'd4;
             end
             else
             begin
+
                 if( ls_min+4'd1 == 4'd10 )
                 begin
                     // ls_minutes overflows to ms_minutes 
@@ -133,59 +186,59 @@ module DISP_DRVR
 
     always @*
     begin
-        next_snooze <= curr_snooze;
-        next_state <= curr_state;
-        int_sound_alarm <= 1'b0;
+//        next_snooze <= curr_snooze;
+//        next_state <= curr_state;
+//        int_sound_alarm <= 1'b0;
 
         case( curr_state ) 
             STATE_WAIT_FOR_ALARM :
             begin
                 if( current_time==alarm_time ) begin
-                    next_state <= STATE_ALARM_RINGING;
+                    next_state = STATE_ALARM_RINGING;
                 end
             end 
 
             STATE_ALARM_RINGING :
             begin
-                int_sound_alarm <= 1'b1;
+                int_sound_alarm = 1'b1;
                 if( stop_alarm == 1'b1 ) begin
-                    next_state <= STATE_ALARM_OFF;
+                    next_state = STATE_ALARM_OFF;
                 end
                 else 
                 if( do_snooze ) begin
-                    next_state <= STATE_SNOOZE_ACTIVATED;
+                    next_state = STATE_SNOOZE_ACTIVATED;
                 end
             end 
 
             STATE_ALARM_OFF :
             begin
-                int_sound_alarm <= 1'b0;
-                next_state <= STATE_WAIT_FOR_ALARM;
+                int_sound_alarm = 1'b0;
+                next_state = STATE_WAIT_FOR_ALARM;
             end 
 
             STATE_SNOOZE_ACTIVATED :
             begin
-                int_sound_alarm <= 1'b0;
+                int_sound_alarm = 1'b0;
                 // add five minutes to snooze time brute force badly
                 bcd_clock_minute( current_time[15:12], current_time[11:8],
                                     current_time[7:4], current_time[3:0],
                                   next_snooze[15:12], next_snooze[11:8],
                                   next_snooze[7:4], next_snooze[3:0] );
-                next_state <= STATE_WAIT_FOR_SNOOZE;
+                next_state = STATE_WAIT_FOR_SNOOZE;
             end 
 
             STATE_WAIT_FOR_SNOOZE :
             begin
                 if( current_time==curr_snooze ) begin
-                    next_state <= STATE_ALARM_RINGING;
-                    next_snooze <= 16'd0;
+                    next_state = STATE_ALARM_RINGING;
+                    next_snooze = 16'd0;
                 end 
             end 
 
             default :
             begin
-                next_state <= STATE_WAIT_FOR_ALARM;
-                next_snooze <= 16'd0;
+                next_state = STATE_WAIT_FOR_ALARM;
+                next_snooze = 16'd0;
             end 
 
         endcase
