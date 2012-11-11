6cccc2ab45c7149c48849af52628c4a1568b1cd5
David Poole <testcluster@gmail.com>
2012-11-11 09:30:53 -0700

    rename incoming clock from clk256 to clk; remove some commented out code

--- al_clk_counter.v
+++ al_clk_counter.v
@@ -1,7 +1,7 @@
 `timescale 1 ns / 10 ps
 
 module al_clk_counter
-    ( input clk256,
+    ( input clk,
       input reset,
       input one_minute,
       input [15:0] time_in,  /* time in BCD format */
@@ -9,20 +9,10 @@ module al_clk_counter
 
       output [15:0] current_time_out );
 
-//    reg [3:0] bcd_ms_hour = 0;
-//    reg [3:0] bcd_ls_hour = 0;
-//    reg [3:0] bcd_ms_min = 0;
-//    reg [3:0] bcd_ls_min = 0;
-
     reg [3:0] bcd_ms_hour_out;
     reg [3:0] bcd_ls_hour_out;
     reg [3:0] bcd_ms_min_out;
     reg [3:0] bcd_ls_min_out;
-
-//    reg [15:0] int_current_time;
-
-//    reg [15:0] int_next_time;
-
 
     task bcd_clock_minute; 
           input one_minute;
@@ -82,23 +72,6 @@ module al_clk_counter
     end
     endtask
 
-    /* 
-     * increments current time by one minute 
-     */
-//    bcd_clock run_bcd_clock
-//        (.clk(clk256),
-//         .add_one(one_minute),
-//         .ms_hour(int_current_time[15:12]),
-//         .ls_hour(int_current_time[11:8]),
-//         .ms_min(int_current_time[7:4]),
-//         .ls_min(int_current_time[3:0]),
-//         
-//         .out_ms_hour(bcd_ms_hour_out),
-//         .out_ls_hour(bcd_ls_hour_out),
-//         .out_ms_min(bcd_ms_min_out),
-//         .out_ls_min(bcd_ls_min_out)
-//    );
-
     reg [15:0] int_current_time;
 
     assign current_time_out = int_current_time;
@@ -106,7 +79,7 @@ module al_clk_counter
 //    assign current_time_out = { bcd_ms_hour_out, bcd_ls_hour_out, 
 //                                  bcd_ms_min_out, bcd_ls_min_out };
 
-    always @(posedge(reset),posedge(clk256))
+    always @(posedge(reset),posedge(clk))
     begin
         if( reset ) 
         begin
